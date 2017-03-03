3cefcfdb96786bfdb1b0875265054481f4c07f5a
Brian Misachi <misachibrian@gmail.com>
2017-03-03 03:50:33 +0300

    Used decouple to separate setting variables from source code

--- dbconnect.py
+++ dbconnect.py
@@ -1,14 +1,15 @@
 import psycopg2
+from decouple import config  # for separating setting parameters from source code
 
 
 def connect_db():  # function attempts to make a connection with the database
     try:  # and returns a connection object if successful
         conn = psycopg2.connect(
-            database="passwords",
-            user="user",
-            password="pword",
-            host="127.0.0.1",
-            port="5432"
+            database=config("DATABASE"),
+            user=config("USER"),
+            password=config("PASSWORD"),
+            host=config("HOST"),
+            port=config("PORT")
         )
 
         return conn
