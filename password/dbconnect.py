0910fb3da6233691a5ac67fa89497d4d37dd8894
misachi <misachibrian@gmail.com>
2017-04-18 14:22:19 +0300

    Formatted to conform with pep8 rules

--- password/dbconnect.py
+++ password/dbconnect.py
@@ -1,9 +1,10 @@
 import psycopg2
-from decouple import config  # for separating setting parameters from source code
+from decouple import config  # for separating setting
+# parameters from source code
 
 
-def connect_db():  # function attempts to make a connection with the database
-    try:  # and returns a connection object if successful
+def connect_db():  # function attempts to make a connection
+    try:  # with the database and returns a connection object if successful
         conn = psycopg2.connect(
             database=config("DATABASE"),
             user=config("USER"),
